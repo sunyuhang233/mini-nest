4e71a582a13b1d61329684f083a996100315059f
hang <2283582431@qq.com>
2025-11-19 21:56:40 +0800

    feat: 补充类型

--- src/@nestjs/common/nest-interceptor.interface.ts
+++ src/@nestjs/common/nest-interceptor.interface.ts
@@ -1,5 +1,7 @@
+import { Observable } from "rxjs";
+import { CallHandler } from "./call-handler.interface";
 import { ExecutionContext } from "./execution-context.interface";
 
 export interface NestInterceptor<T = any, R = any> {
-  intercept(context: ExecutionContext, next)
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<R>;
 }
--- src/timeout.interceptor.ts
+++ src/timeout.interceptor.ts
@@ -1,10 +1,10 @@
-import { Injectable, NestInterceptor, ExecutionContext, RequestTimeoutException, } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, RequestTimeoutException, CallHandler, } from '@nestjs/common';
 import { Observable, throwError, TimeoutError } from 'rxjs';
 import { catchError, timeout } from 'rxjs/operators';
 
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       timeout(1000),
       catchError(err => {
