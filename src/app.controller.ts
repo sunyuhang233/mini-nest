819c2f30bf3832d3fc44eb05efa476db003078bd
hang <2283582431@qq.com>
2025-11-20 21:46:36 +0800

    feat: 移除多余代码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,33 +1,10 @@
-import { Controller, Get, UseInterceptors } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { Logger1Interceptor } from "./logger1.interceptor";
-import { Logger2Interceptor } from "./logger2.interceptor";
-import { Logging3Interceptor } from "./logger3.interceptor";
-import { Logging4Interceptor } from "./logger4.interceptor";
-import { CacheInterceptor } from './cache.interceptor';
-import { TimeoutInterceptor } from './timeout.interceptor';
 @Controller("app")
-@UseInterceptors(Logging3Interceptor)
-@UseInterceptors(Logging4Interceptor)
 export class AppController {
   constructor(private appService: AppService) { }
   @Get()
-  @UseInterceptors(Logger1Interceptor)
-  @UseInterceptors(Logger2Interceptor)
   getHello(): string {
     return 'hello world';
-  }
-
-  @Get('cache')
-  @UseInterceptors(CacheInterceptor)
-  async cache() {
-    console.log('cache...');
-    return 'cache';
-  }
-
-  @Get('timeout')
-  @UseInterceptors(TimeoutInterceptor)
-  async timeout() {
-    await new Promise(resolve => setTimeout(resolve, 2000));
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from "./@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { Logging6Interceptor } from "./logger6.interceptor";
-import { Logging5Interceptor } from "./logger5.interceptor";
-import { APP_INTERCEPTOR } from "@nestjs/core";
 @Module({
   providers: [
     AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: Logging6Interceptor,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: Logging5Interceptor,
-    }
   ],
   controllers: [AppController],
 })
