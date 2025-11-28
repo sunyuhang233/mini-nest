7b2baad158a610073c50a44362581d203de46f88
hang <2283582431@qq.com>
2025-11-28 20:57:10 +0800

    feat: 移除代码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,10 @@
-import { Body, Controller, Get, UseFilters } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { isPublic } from "./public.metadata";
-import { I18n, I18nContext, I18nValidationExceptionFilter } from "nestjs-i18n";
-import { I18nTranslations } from "./generated/i18n.generated";
-import { Post } from "./@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
 @Controller("app")
 export class AppController {
   constructor(private appService: AppService) { }
   @Get()
-  @isPublic()
-  async getHello(@I18n() i18n: I18nContext<I18nTranslations>): Promise<string> {
-    return await i18n.t('test.HELLO');
+  getHello(): string {
+    return this.appService.getHello();
   }
-
-  @Get("args")
-  @isPublic()
-  async getProduct(@I18n() i18n: I18nContext<I18nTranslations>): Promise<string> {
-    return await i18n.t('test.PRODUCT.NEW', { args: { name: 'NestJS' } });
-  }
-
-  @Post("create-user")
-  @isPublic()
-  createUser(@Body() createUserDto: CreateUserDto): string {
-    return 'create user'
-  }
-
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,12 @@
-import { APP_GUARD } from "@nestjs/core";
 import { Module } from "./@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from './auth.module';
-import { UsersModule } from './users.module';
-import { AuthGuard } from "./auth.guard";
-import { AcceptLanguageResolver, CookieResolver, HeaderResolver, I18nModule, QueryResolver } from "nestjs-i18n";
-import path from "path";
+
 @Module({
   providers: [
     AppService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    }
   ],
   controllers: [AppController],
-  imports: [AuthModule, UsersModule, I18nModule.forRoot({
-    fallbackLanguage: 'en',
-    loaderOptions: {
-      path: path.join(__dirname, '/i18n/'),
-      watch: true,
-    },
-    resolvers: [
-      new QueryResolver(["lang", "l"]),
-      new HeaderResolver(["x-custom-lang"]),
-      new CookieResolver(),
-      AcceptLanguageResolver,
-    ],
-    typesOutputPath: path.join(__dirname, '../src/generated/i18n.generated.ts'),
-  }),],
 })
 export class AppModule { }
+
