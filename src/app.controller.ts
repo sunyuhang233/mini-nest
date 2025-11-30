c85d756da98cfd04982e3fd4cd67475daf6f75ff
hang <2283582431@qq.com>
2025-11-30 14:37:03 +0800

    feat: 移除多余代码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,10 @@
-import { Controller, Get, Logger } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
 @Controller("app")
 export class AppController {
   constructor(private appService: AppService) { }
-  // 带时间戳  { timestamp: true }
-  private readonly logger = new Logger(AppController.name);
   @Get()
   getHello(): string {
-    this.logger.error("getHello error");
     return this.appService.getHello();
   }
 }
